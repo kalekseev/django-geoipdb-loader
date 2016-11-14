5734f409ed48f0069ca30a483ccb088fde4eef19
Konstantin Alekseev <kv.alekseev@gmail.com>
2016-11-14 13:52:00 +0300

    Accept download args in celery task

--- src/geoipdb_loader/tasks.py
+++ src/geoipdb_loader/tasks.py
@@ -7,5 +7,10 @@ logger = get_task_logger(__name__)
 
 
 @shared_task(ignore_result=True)
-def update_geoipdb():
-    download(logger=logger)
+def update_geoipdb(skip_city=False, skip_country=False, skip_md5=False):
+    download(
+        skip_city=skip_city,
+        skip_country=skip_country,
+        skip_md5=skip_md5,
+        logger=logger,
+    )
